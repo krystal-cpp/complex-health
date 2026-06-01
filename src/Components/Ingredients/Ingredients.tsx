1ceee2eb00f96bdc0c0f4f8a44b87b4c1ce3cceb
krystal <kuzmenok-vlad@mail.ru>
2026-06-01 22:23:47 +0300

    added ingredients and price components
    added adaptive components

---
+++ src/Components/Ingredients/Ingredients.tsx
@@ -0,0 +1,137 @@
+export default function Ingredients() {
+    return (
+        <div id='ingredients' className='px-4 xl:px-10 pt-16'>
+            <div className='bg-white p-4 xl:p-10 rounded-4xl xl:flex'>
+                <div className='hidden xl:flex'>
+                    <img loading='lazy' className='w-180 h-200 shrink-0' src='/big-spoon.webp' alt='Состав БАДа Суставы COMPLEX SW Оптисалт' />
+                </div>
+
+                <h2 className='block xl:hidden text-black font-medium text-3xl/9'>
+                    «Complex SW СУСТАВЫ» — комплекс <span className='text-[#3496CE]'>из 9 активных компонентов</span>
+                </h2>
+
+                <div className='py-4 flex xl:hidden'>
+                    <img loading='lazy' className='' src='/spoon.webp' alt='Состав БАДа Суставы COMPLEX SW Оптисалт' />
+                </div>
+
+                <div className='hidden xl:flex flex-col px-20 py-25 gap-10'>
+                    <h2 className=' text-black font-medium text-3xl/9'>
+                        «Complex SW СУСТАВЫ» — комплекс <span className='text-[#3496CE]'>из 9 активных компонентов</span>
+                    </h2>
+
+                    <div className='hidden xl:grid xl:grid-cols-2 xl:gap-x-6'>
+                        <div className='flex flex-col gap-8'>
+                            <div className='flex flex-col gap-1'>
+                                <h3 className='text-black font-medium text-2xl'>Глюкозамин</h3>
+                                <p className='text-[#808080] text-base/6'>Хондропротектор, который восстанавливает хрящевую ткань, уменьшает боль и воспаление в суставах, а также улучшает их подвижность</p>
+                            </div>
+                            <div className='flex flex-col gap-1'>
+                                <h3 className='text-black font-medium text-2xl'>МСМ</h3>
+                                <p className='text-[#808080] text-base/6'>Метилсульфонилметан укрепляет суставы, снимает воспаление и боль, улучшает кожу, волосы, ногти и нормализует иммунитет</p>
+                            </div>
+                            <div className='flex flex-col gap-1'>
+                                <h3 className='text-black font-medium text-2xl'>Босвелия</h3>
+                                <p className='text-[#808080] text-base/6'>Подавляет воспаление в суставах, блокируя синтез лейкотриенов, что уменьшает боль и защищает хрящ от разрушения без вреда для ЖКТ</p>
+                            </div>
+                            <div className='flex flex-col gap-1'>
+                                <h3 className='text-black font-medium text-2xl'>Витамин Е</h3>
+                                <p className='text-[#808080] text-base/6'>Защищает суставы, замедляя разрушение хряща  и уменьшая боль и скованность</p>
+                            </div>
+                            <div className='flex flex-col gap-1'>
+                                <h3 className='text-black font-medium text-2xl'>Селен</h3>
+                                <p className='text-[#808080] text-base/6'>Замедляет старение как антиоксидант и сохраняет эластичность тканей</p>
+                            </div>
+                        </div>
+                        <div className='flex flex-col gap-8'>
+                            <div className='flex flex-col gap-1'>
+                                <h3 className='text-black font-medium text-2xl'>Хондроитин</h3>
+                                <p className='text-[#808080] text-base/6'>Является структурным компонентом хряща, который защищает его от разрушения, улучшает минеральный обмен и стимулирует выработку суставной жидкости.</p>
+                            </div>
+                            <div className='flex flex-col gap-1'>
+                                <h3 className='text-black font-medium text-2xl'>Коллаген</h3>
+                                <p className='text-[#808080] text-base/6'>Является основным фибриллярным белком соединительной ткани, обеспечивающим прочность и эластичность сухожилий, костей и хрящей</p>
+                            </div>
+                            <div className='flex flex-col gap-1'>
+                                <h3 className='text-black font-medium text-2xl'>Витамин С</h3>
+                                <p className='text-[#808080] text-base/6'>Участвует в синтезе коллагена, метаболизме гормонов и тирозина, окислительно-восстановительных процессах и улучшает усвоение железа в кишечнике</p>
+                            </div>
+                            <div className='flex flex-col gap-1'>
+                                <h3 className='text-black font-medium text-2xl'>Марганец</h3>
+                                <p className='text-[#808080] text-base/6'>Необходим для работы центральной нервной системы, крепости костей и жирового обмена</p>
+                            </div>
+                            <div className='flex'>
+                                <a aria-label='Купить в 1 клик' href='#price-card' className='bg-[#3193CC] rounded-4xl px-6 py-2 text-white text-2xl font-medium'>
+                                    Купить в 1 клик
+                                </a>
+                                <a aria-label='Купить в 1 клик' href='#price-card' className='bg-[#3193CC] rounded-full w-14 h-14 flex items-center justify-center'>
+                                    <svg
+                                        className='w-8 h-8 text-white transform -rotate-45 ml-1 mb-1'
+                                        fill='none'
+                                        stroke='currentColor'
+                                        viewBox='0 0 24 24'>
+                                        <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M9 5l7 7-7 7 M16 12l-12 0' />
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className='flex xl:hidden flex-col gap-4'>
+                    <div className='flex flex-col gap-1'>
+                        <h3 className='text-black font-medium text-xl xl:text-2xl'>Глюкозамин</h3>
+                        <p className='text-[#808080] text-sm/4 xl:text-base/6'>Хондропротектор, который восстанавливает хрящевую ткань, уменьшает боль и воспаление в суставах, а также улучшает их подвижность</p>
+                    </div>
+                    <div className='flex flex-col gap-1'>
+                        <h3 className='text-black font-medium text-xl xl:text-2xl'>Хондроитин</h3>
+                        <p className='text-[#808080] text-sm/4'>Является структурным компонентом хряща, который защищает его от разрушения, улучшает минеральный обмен и стимулирует выработку суставной жидкости.</p>
+                    </div>
+                    <div className='flex flex-col gap-1'>
+                        <h3 className='text-black font-medium text-xl xl:text-2xl'>МСМ</h3>
+                        <p className='text-[#808080] text-sm/4'>Метилсульфонилметан укрепляет суставы, снимает воспаление и боль, улучшает кожу, волосы, ногти и нормализует иммунитет</p>
+                    </div>
+                    <div className='flex flex-col gap-1'>
+                        <h3 className='text-black font-medium text-xl xl:text-2xl'>Коллаген</h3>
+                        <p className='text-[#808080] text-sm/4'>Является основным фибриллярным белком соединительной ткани, обеспечивающим прочность и эластичность сухожилий, костей и хрящей</p>
+                    </div>
+                    <div className='flex flex-col gap-1'>
+                        <h3 className='text-black font-medium text-xl xl:text-2xl'>Босвелия</h3>
+                        <p className='text-[#808080] text-sm/4'>Подавляет воспаление в суставах, блокируя синтез лейкотриенов, что уменьшает боль и защищает хрящ от разрушения без вреда для ЖКТ</p>
+                    </div>
+                    <div className='flex flex-col gap-1'>
+                        <h3 className='text-black font-medium text-xl xl:text-2xl'>Витамин С</h3>
+                        <p className='text-[#808080] text-sm/4'>Участвует в синтезе коллагена, метаболизме гормонов и тирозина, окислительно-восстановительных процессах и улучшает усвоение железа в кишечнике</p>
+                    </div>
+                    <div className='flex flex-col gap-1'>
+                        <h3 className='text-black font-medium text-xl xl:text-2xl'>Витамин Е</h3>
+                        <p className='text-[#808080] text-sm/4'>Защищает суставы, замедляя разрушение хряща  и уменьшая боль и скованность</p>
+                    </div>
+                    <div className='flex flex-col gap-1'>
+                        <h3 className='text-black font-medium text-xl xl:text-2xl'>Марганец</h3>
+                        <p className='text-[#808080] text-sm/4'>Необходим для работы центральной нервной системы, крепости костей и жирового обмена</p>
+                    </div>
+                    <div className='flex flex-col gap-1'>
+                        <h3 className='text-black font-medium text-xl xl:text-2xl'>Селен</h3>
+                        <p className='text-[#808080] text-sm/4'>Замедляет старение как антиоксидант и сохраняет эластичность тканей</p>
+                    </div>
+                </div>
+
+                <div className='flex xl:hidden pt-4'>
+                    <a aria-label='Заказать' href='#price-card' className='bg-[#3193CC] rounded-4xl px-4 py-1.5 text-white text-base font-medium'>
+                        Заказать
+                    </a>
+                    <a aria-label='Заказать' href='#price-card' className='bg-[#3193CC] rounded-full w-10 h-10 flex items-center justify-center'>
+                        <svg
+                            className='w-6 h-6 text-white transform -rotate-45 ml-1 mb-1'
+                            fill='none'
+                            stroke='currentColor'
+                            viewBox='0 0 24 24'>
+                            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M9 5l7 7-7 7 M16 12l-12 0' />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+}
